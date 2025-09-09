0d64af4a062cd2190d414821fad8bced944d01e5
AustinWells8 <ajw272@miami.edu>
2025-09-09 15:46:50 -0400

    Display of simple plot of data_lionfish showing total fish weight by length for each type.

---
+++ assignment_build_your_portfolio.R
@@ -0,0 +1,26 @@
+#HW#1 - Build Your Portfolio
+
+# Load packages
+library(EVR628tools)
+library(tidyverse)
+
+data("data_lionfish")
+
+?data_lionfish
+
+# Load data
+data(data_lionfish)
+
+# Create a simple plot
+p <- ggplot(data_lionfish,
+            aes(x = total_length_mm, y = total_weight_gr)) +
+  geom_point() +
+  facet_wrap(~site)
+
+p
+
+# Save plot
+# The above shouldn't work, why?
+ggsave(plot = p, filename = "results/figures/first_plot.png")
+
+create.dir = TRUE
